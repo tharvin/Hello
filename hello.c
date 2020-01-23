1dcf6f954921fc9d986445bd34457ac644f92bed
tharvin <54672733+tharvin@users.noreply.github.com>
2020-01-23 00:29:57 -0600

    Adding print line for major

---
+++ hello.c
@@ -0,0 +1,22 @@
+
+
+
+/*
+  Programmer's Name: Tharvin Naidu R Selvasingam	
+  NUID: 28439453
+  Date: 24/01/2020 
+  Description of the program: A simple hello world style program
+  
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+ // Having print my name and major
+
+ printf("Tharvin\n");
+ printf("Civil Engineering\n");
+  return 0;
+}
